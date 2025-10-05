68affe8d809e9e9f027a00a6372a4721aa631369
minhtai2911 <149917860+minhtai2911@users.noreply.github.com>
2025-10-06 01:42:51 +0700

    fix/auth-check-isActive-hide-admin

--- services/user-service/prisma/seed.ts
+++ services/user-service/prisma/seed.ts
@@ -1,17 +1,19 @@
 import { PrismaClient } from "../generated/prisma/client.js";
 import bcrypt from "bcrypt";
 import logger from "../src/utils/logger.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 const prisma = new PrismaClient();
 
 async function main() {
-  const hashedPassword = await bcrypt.hash("admin@123", 10);
+  const hashedPassword = await bcrypt.hash(`${process.env.ADMIN_PASSWORD}`, 10);
 
   const admin = await prisma.user.upsert({
-    where: { email: "admin@gmail.com" },
+    where: { email: `${process.env.ADMIN_EMAIL}` },
     update: {},
     create: {
-      email: "admin@gmail.com",
+      email: `${process.env.ADMIN_EMAIL}`,
       password: hashedPassword,
       fullname: "Admin",
       gender: "MALE",
