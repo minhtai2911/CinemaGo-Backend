927e0ff00efbd6279354d60939849c66f9e29593
minhtai2911 <149917860+minhtai2911@users.noreply.github.com>
2025-12-21 22:20:39 +0700

    fix: change GET to POST for /public/batch route in showtime.routes.ts

--- services/showtime-service/src/routes/showtime.routes.ts
+++ services/showtime-service/src/routes/showtime.routes.ts
@@ -34,6 +34,6 @@ router.put(
   authorizeRole("MANAGER", "ADMIN"),
   ShowtimeController.restoreShowtimeById
 );
-router.get("/public/batch", ShowtimeController.getShowtimesByIds);
+router.post("/public/batch", ShowtimeController.getShowtimesByIds);
 
 export default router;
